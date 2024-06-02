9fb3e8203f953d101d6567f0f688a95de5188526
Taeuk Ahn <tu1014@naver.com>
2024-06-02 16:10:44 +0900

    Update uart.m

--- uart.m
+++ uart.m
@@ -1,6 +1,9 @@
 
 device = serialport("COM6", 115200);
-configureTerminator(device, "CR/LF");
+% configureTerminator(device, "CR/LF");
+
+configureTerminator(device, "LF");
+
 
 Pwr_Dmd = 0.5;
 Cap_SOC = 0.8;
@@ -13,11 +16,11 @@ Bat_SOC = int8(Bat_SOC*100);
 input = [ int2str(Pwr_Dmd) ',' int2str(Cap_SOC) ',' int2str(Bat_SOC)];
 input_b = uint8(input);
 
-% writeline(device, input);
+writeline(device, input);
 
-writeline(device, int2str(Pwr_Dmd));
-writeline(device, int2str(Cap_SOC));
-writeline(device, int2str(Bat_SOC));
+% writeline(device, int2str(Pwr_Dmd));
+% writeline(device, int2str(Cap_SOC));
+% writeline(device, int2str(Bat_SOC));
 flush(device);
 
 % res = readline(device);
